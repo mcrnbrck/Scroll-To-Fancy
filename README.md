33e42a0463730fca33551da20df045f1413a67fb
Martin Crnbrck <martin@blakedigital.com>
2018-10-29 11:31:39 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # Scroll-To-Fancy
 No dependency, super simple background image parallax and scroll to reveal effects for your website.
+
+**Demo**
+[martinc.de/scroll-to-fancy](http://martinc.de/scroll-to-fancy/)
+
+
+## Easy to Install
+Just add scroll-to-fancy.min.css and scroll-to-fancy.min.js to your website, then call ScrollToFancy().
+
+```html
+<link rel="stylesheet" href="/css/scroll-to-fancy.min.css">    
+
+<script src="/js/jquery.scroll-to-fancy.min.js"></script>
+<script>
+  ScrollToFancy({
+    // optional settings
+    parallax:               true,
+    parallaxMinWidth:       800,
+    scrollToReveal:         true,
+    scrollToRevealMinWidth: 800,
+    scrollToRevealOffset:   140
+  });
+</script>
+```
+
+Scroll To Fancy is written in JavaScript, has no dependencies and works in Firefox, Chrome, Safari, Edge and IE 11.
+
+## Parallax effect for background images
+Add a parallax effect to any html element with a background image. Just add data-parallax="0.5" as an attribute to get started.
+
+```html
+<div class="background" data-parallax="0.5"></div>
+```
+
+Modify the scroll speed of your parallax background image
+
+- 0.9 = almost same speed as user scrolls (fast)
+- 0.5 = half of the scroll speed
+- 0.1 = almost fixed (slow)
+
+## Scroll to reveal effect for any element
+
+```html
+<div class="example" data-reveal="fadeInUp"></div>
+```
+
+**Choose the fade in animation**
+Enter the name of the CSS Keyframe animation as the value of data-reveal.
+You can use the following animations or create your own:
+
+- .fadeIn
+- .fadeInLeft
+- .fadeInRight
+- .fadeInUp
+- .fadeInUpBig
+- .fadeInDown
+- .fadeInDownBig
+- .zoomIn
+
+**Control the delay** (optional)
+Choose the delay in milliseconds when the element fades in. This comes in handy when multiple elements enter the viewport at the same time.
+
+```html
+<div class="box-1" data-reveal="fadeInUp" data-reveal-delay="400"></div>
+<div class="box-2" data-reveal="fadeInUp" data-reveal-delay="800"></div>
+<div class="box-3" data-reveal="fadeInUp" data-reveal-delay="1200"></div>
+```
+
+[View Demo](http://martinc.de/scroll-to-fancy/)
---
+++ js/scroll-to-fancy.js
@@ -0,0 +1,254 @@
+var ScrollToFancy = function(Options) {
+  var _settings = {
+    parallax:               true,
+    parallaxMinWidth:       800,
+    scrollToReveal:         true,
+    scrollToRevealMinWidth: 800,
+    scrollToRevealOffset:   140
+  };
+  var _scrollDistance = 0;
+
+
+  /************************************************************
+  FUNCTION: init()
+  DESC:     assign user options to settings and assign events
+  ************************************************************/
+  this.init = function(Options) {
+
+    if ('parallax' in Options && typeof(Options.parallax) === 'boolean') {
+      _settings.parallax = Options.parallax;
+    }
+
+    if ('parallaxMinWidth' in Options && typeof(Options.parallaxMinWidth) === 'number') {
+      _settings.parallaxMinWidth = Options.parallaxMinWidth;
+    }
+
+    if ('scrollToReveal' in Options && typeof(Options.scrollToReveal) === 'boolean') {
+      _settings.scrollToReveal = Options.scrollToReveal;
+    }
+
+    if ('scrollToRevealMinWidth' in Options && typeof(Options.scrollToRevealMinWidth) === 'number') {
+      _settings.scrollToRevealMinWidth = Options.scrollToRevealMinWidth;
+    }
+
+    if ('scrollToRevealOffset' in Options && typeof(Options.scrollToRevealOffset) === 'number') {
+      _settings.scrollToRevealOffset = Options.scrollToRevealOffset;
+    }
+
+    window.addEventListener('scroll', function() {
+      window.requestAnimationFrame( function() {
+        _scrollDistance = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+      });
+    });
+
+    if (_settings.parallax === true) {
+      _assignParallaxScrolling();
+    }
+
+    if (_settings.scrollToReveal === true) {
+      _assignScrollToReveal();
+    }
+  } //END init()
+
+
+  /************************************************************
+  FUNCTION: _assignScrollToReveal()
+  DESC:     fade in elements when user scrolls to it
+  ************************************************************/
+  function _assignScrollToReveal() {
+    var animationWrapperClass = 'scroll-to-reveal';
+    var revealOffset          = _settings.scrollToRevealOffset; // threshold before element fades in
+    var animationWrapperElem  = document.body;
+    var animationElems        = document.querySelectorAll('[data-reveal]:not(.scroll-to-reveal-hide)');
+    var triggers              = [];
+
+
+    /************************************************************
+    FUNCTION: assignAnimationTriggers()
+    DESC:     get all elements that have a scroll to reveal effect
+    ************************************************************/
+    var assignAnimationTriggers = function() {
+      triggers = [];
+
+      if (window.innerWidth >= _settings.scrollToRevealMinWidth) {
+        animationWrapperElem.classList.add( animationWrapperClass );
+      } else {
+        animationWrapperElem.classList.remove( animationWrapperClass );
+        return false;
+      }
+
+      for (var i=0; i<animationElems.length; i++) {
+        var item = animationElems[i];
+
+        item.classList.add('scroll-to-reveal-hide');  
+
+        // save element
+        triggers.push({
+          revealed:   false,
+          element:    item,
+          offset:     Math.round( item.getBoundingClientRect().top + document.documentElement.scrollTop ),
+          animation:  item.getAttribute('data-reveal'),
+          delay:      parseInt( item.getAttribute('data-reveal-delay') )
+        });
+
+      }
+
+      // console.log(animationElems);
+    } // END assignAnimationTriggers()
+
+
+    /************************************************************
+    FUNCTION: playAnimation()
+    DESC:     check if element is in viewport via scroll distance
+    ************************************************************/
+    var playAnimation = function() {
+
+      if (!animationWrapperElem.classList.contains('scroll-to-reveal') || triggers.length === 0) {
+        return false;
+      }
+
+      var scrollOffset = Math.round( _scrollDistance + (window.innerHeight - revealOffset) );
+
+      for (var i=0; i<triggers.length; i++) {
+        var item = triggers[i];
+
+        if (!item.revealed && item.offset < scrollOffset && item.offset > (scrollOffset - window.innerHeight)) {
+
+          // check for delay
+          if (item.delay) item.element.style.animationDelay = item.delay + 'ms';
+
+          // assign reveal class
+          item.element.classList.add( item.animation );
+          triggers[i].revealed = true;
+        }
+      };
+      
+      // todo remove item from triggers?
+
+    } // playAnimation()
+
+
+    assignAnimationTriggers();
+    playAnimation();
+   
+    // get and save animation triggers on resize
+    window.addEventListener('resize', function() {
+      window.requestAnimationFrame( assignAnimationTriggers );
+      window.requestAnimationFrame( playAnimation );
+    });
+
+    // play fade in animation on scroll
+    window.addEventListener('scroll', function() {
+      window.requestAnimationFrame( playAnimation );
+    });
+  } // _assignScrollToReveal()
+
+
+
+
+  /************************************************************
+  FUNCTION: _assignParallaxScrolling()
+  DESC:     assigns hero image parallax scrolling
+  ************************************************************/
+  function _assignParallaxScrolling() {
+    var classEnabled    = 'scroll-to-parallax';
+    var elems           = document.querySelectorAll('[data-parallax]');
+    var scrollModifier  = 0.5; /* (slow) 0.01, 0.99 (fast) */
+    var triggers        = [];
+
+    if (!elems) {
+      return false;
+    }
+
+    /************************************************************
+    FUNCTION: getElementTriggers()
+    DESC:     save all elements with position values in array
+    ************************************************************/
+    var getElementTriggers = function() {
+      triggers = [];
+
+      if (window.innerWidth >= _settings.parallaxMinWidth) {
+        document.body.classList.add(classEnabled);
+      } else {
+        document.body.classList.remove(classEnabled);
+
+        // remove bg position and leave triggers empty when window width is to small
+        for (var i=0; i<elems.length; i++) {
+          var item = elems[i];
+
+          item.classList.remove('has-parallax');
+          item.style.backgroundPosition = '';
+        };
+
+        return false;
+      }
+
+      // save all the elements in triggers
+      for (var i=0; i<elems.length; i++) {
+        var item = elems[i];
+
+        item.classList.add('has-parallax');
+
+        // get scroll speed modifier from element (use global scroll speed modifier as a fallback)
+        if (isNaN(scrollSpeed = parseFloat(item.getAttribute('data-parallax')))) {
+          scrollSpeed = scrollModifier;
+        }
+
+        // save elements
+        triggers.push({
+          element:      item,
+          offset:       Math.round( item.getBoundingClientRect().top + document.documentElement.scrollTop ),
+          height:       item.offsetHeight,
+          position:     (item.hasAttribute('data-parrallax-position') ? item.getAttribute('data-parrallax-position') : 'center'),
+          speed:        scrollSpeed
+        });
+      }
+    } //END getElementTriggers()
+
+
+    /************************************************************
+    FUNCTION: playAnimation()
+    DESC:     check if element is in viewport and animate background
+    ************************************************************/
+    var playAnimation = function() {
+      var revealOffset = 1000;
+
+      if (triggers.length === 0) {
+        return false;
+      }
+
+      var scrollOffset = Math.round( _scrollDistance + (window.innerHeiht - revealOffset) * 100) / 100;
+
+      for (var i=0; i<triggers.length; i++) {
+        var item = triggers[i];
+
+        // check if item is in viewport
+        if ((window.innerHeight + _scrollDistance) > item.offset && _scrollDistance < (item.offset + item.height)) {
+          // calculate vertical position
+          var y = (_scrollDistance - item.offset) * item.speed;
+          // set background position
+          item.element.style.backgroundPosition = 'center ' + y + 'px';
+        }
+      }
+    } // playAnimation()
+
+
+    getElementTriggers();
+    playAnimation();
+
+    // get and save element positions on resize
+    window.addEventListener('resize', function() {
+      window.requestAnimationFrame( getElementTriggers );
+      window.requestAnimationFrame( playAnimation );
+    });
+
+    // play parallax animation on scroll
+    window.addEventListener('scroll', function() {
+      window.requestAnimationFrame( playAnimation );
+    });
+
+  } //END _assignParallaxScrolling()
+
+
+  init(Options);
+}
